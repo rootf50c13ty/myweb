da1b00dd6cb0e3b48a048544f007de21081e3613
elliot alderson <r00t4f50c13ty@gmail.com>
2019-12-18 18:26:59 +0530

    Nav Bar

--- home.php
+++ home.php
@@ -5,16 +5,22 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
+		<link href="/home/ajin/lampstack-7.3.12-0/apache2/htdocs/myweb/css/navbar.css" rel="stylesheet">
+
+
+
+
 		<style>
 /*
 			<link rel="stylesheet" type="text/css"
           href="/home/ajin/lampstack-7.3.12-0/apache2//htdocs/myweb/css/style.css"/> */
 			 
 			/* Style the header */
-				.header {
-				  padding: 80px;
+				.header 
+				{
+				  padding: 50px;
 				  text-align: center;
-				  background: #ACA5A5;
+				  background: #333;
 				  color: white;
 				}
 
@@ -28,33 +34,81 @@
 				  src: local('MrRobot'), url('../css/MR ROBOT.woff') format('woff'); */
 				}
 
+				* {box-sizing: border-box;}
 
-				.navbar {
-  overflow: hidden; /* Hide overflow */
-  background-color: #333; /* Dark background color */
-}
+				body {
+				  margin: 0;
+				  font-family: Arial, Helvetica, sans-serif;
+				}
 
-/* Style the navigation bar links */
-.navbar a {
-  float: left; /* Make sure that the links stay side-by-side */
-  display: block; /* Change the display to block, for responsive reasons (see below) */
-  color: white; /* White text color */
-  text-align: center; /* Center the text */
-  padding: 14px 20px; /* Add some padding */
-  text-decoration: none; /* Remove underline */
-}
+				.topnav {
+				  overflow: hidden;
+				  background-color: #e9e9e9;
+				}
 
-/* Right-aligned link */
-.navbar a.right {
-  float: right; /* Float a link to the right */
-}
+				.topnav a {
+				  float: left;
+				  display: block;
+				  color: black;
+				  text-align: center;
+				  padding: 14px 16px;
+				  text-decoration: none;
+				  font-size: 17px;
+				}
 
-/* Change color on hover/mouse-over */
-.navbar a:hover {
-  background-color: #ddd; /* Grey background color */
-  color: black; /* Black text color */
-}
-			
+				.topnav a:hover {
+				  background-color: #ddd;
+				  color: black;
+				}
+
+				.topnav a.active {
+				  background-color: #2196F3;
+				  color: white;
+				}
+
+				.topnav .search-container {
+				  float: right;
+				}
+
+				.topnav input[type=text] {
+				  padding: 6px;
+				  margin-top: 8px;
+				  font-size: 17px;
+				  border: none;
+				}
+
+				.topnav .search-container button {
+				  float: right;
+				  padding: 6px 10px;
+				  margin-top: 8px;
+				  margin-right: 16px;
+				  background: #ddd;
+				  font-size: 17px;
+				  border: none;
+				  cursor: pointer;
+				}
+
+				.topnav .search-container button:hover {
+				  background: #ccc;
+				}
+
+				@media screen and (max-width: 600px) {
+				  .topnav .search-container {
+				    float: none;
+				  }
+				  .topnav a, .topnav input[type=text], .topnav .search-container button {
+				    float: none;
+				    display: block;
+				    text-align: left;
+				    width: 100%;
+				    margin: 0;
+				    padding: 14px;
+				  }
+				  .topnav input[type=text] {
+				    border: 1px solid #ccc;  
+				  }
+				}
+
 		
 		</style>
 
@@ -68,12 +122,21 @@
 		</div>
 
 
-		<div class="navbar">
-		  <a href="#">Home</a>
-		  <a href="#">Sign In</a>
-		  <a href="#">Register</a>
-		  <a href="#" class="right">About Us</a>
-		</div>
+		
+	<div class="topnav">
+	  <a class="active" href="#home">Home</a>
+	  <a href="#about">About</a>
+	  <a href="#">Sign Up</a>
+	   <a href="#">Sign In</a>
+	    <a href="#">Contact</a>
+	  <div class="search-container">
+	    <form action="/action_page.php">
+	      <input type="text" placeholder="Search.." name="search">
+	      <button type="submit">Submit</button>
+	    </form>
+	  </div>
+	</div>
+
 
 	</body>
 </html>

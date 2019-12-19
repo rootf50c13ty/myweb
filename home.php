e5a49856756547eb65ed0abbab4c3acb898ccc9c
elliot alderson <r00t4f50c13ty@gmail.com>
2019-12-19 09:40:20 +0530

    home page footer

--- home.php
+++ home.php
@@ -106,7 +106,30 @@
 				  }
 				}
 
+								/* Main column */
+				.main {   
+				  flex: 70%;
+				  background-color: white;
+				  padding: 20px;
+				}
+
+				/* Fake image, just for this example */
+				.fakeimg {
+				  background-color: #aaa;
+				  width: 100%;
+				  padding: 20px;
+				}
 		
+				/* Footer */
+				.footer {
+				  padding: 20px;
+				  text-align: center;
+				  background: #ddd;
+				  margin-top: 20px;
+				}
+
+
+
 		</style>
 
 	</head>
@@ -133,6 +156,24 @@
 	    </form>
 	  </div>
 	</div>
+
+	<div class="main">
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Dec 7, 2017</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    <br>
+    <h2>TITLE HEADING</h2>
+    <h5>Title description, Sep 2, 2017</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+  </div>
+
+  	<div class="footer">
+  <h2>Footer</h2>
+</div>
 
 
 	</body>

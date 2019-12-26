b8ee31c06391cf82950fdff59ecc6c7e0f5f7d67
elliot alderson <r00t4f50c13ty@gmail.com>
2019-12-26 19:18:23 +0530

    thursday

--- js/mailvalidation.js
+++ js/mailvalidation.js
@@ -1,15 +1,33 @@
-function ValidateEmail(inputText)
+function ValidateEmail(inputText,pass1,pass2)
 {
 	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	
 	if(inputText.value.match(mailformat))
 	{
-		document.form1.text1.focus();
+		document.regform.email.focus();
+		
+
+
+		if(pass1.value.length || pass2.value.length == 0)
+		{
+			alert("error message");
+			return false;
+		}
+
+		else
+		{
+			return true;
+		}
+
 		return true;
 	}
 	else
 	{
 		alert("You have entered an invalid email address!");
-		document.form1.text1.focus();
+		document.regform.email.focus();
 		return false;
 	}
+
+
 }
+
--- signup.php
+++ signup.php
@@ -17,158 +17,92 @@
 
         <script>
            
+            function formValidation()
+            {
+                
+                var uname = document.forms["regform"]["uname"];               
+                var email = document.forms["regform"]["email"];    
+                var pass = document.forms["regform"]["pass"];  
+                
+                if (uname.value == "")                                  
+                { 
+                    window.alert("Please enter your name."); 
+                    name.focus(); 
+                    return false; 
+                } 
+               
+                if (address.value == "")                               
+                { 
+                    window.alert("Please enter your address."); 
+                    address.focus(); 
+                    return false; 
+                } 
+                   
+                if (email.value == "")                                   
+                { 
+                    window.alert("Please enter a valid e-mail address."); 
+                    email.focus(); 
+                    return false; 
+                } 
+               
+                if (phone.value == "")                           
+                { 
+                    window.alert("Please enter your telephone number."); 
+                    phone.focus(); 
+                    return false; 
+                } 
+               
+                if (password.value == "")                        
+                { 
+                    window.alert("Please enter your password"); 
+                    password.focus(); 
+                    return false; 
+                } 
+               
+                if (what.selectedIndex < 1)                  
+                { 
+                    alert("Please enter your course."); 
+                    what.focus(); 
+                    return false; 
+                } 
+               
+                return true;
+            }
+
+
             function checkPass()
             {
-                //toggle visibility
-                if(document.getElementById('warning').style.display == 'none')
-                    document.getElementById('warning').style.display = 'block';
-                
-                if(document.getElementById('match').style.display == 'none')
-                    document.getElementById('match').style.display = 'block';
-                
-                if(document.getElementById('match-desc').style.display == 'none')
-                    document.getElementById('match-desc').style.display = 'block';
-                
-                if (document.getElementById('password').value==document.getElementById('repassword').value)  
-                {
-                    document.getElementById('match').style.color='#28a745';
-                    document.getElementById('match').innerHTML='Passwords match';
-                    document.getElementById('match-desc').style.display = 'none';
-                }
-                else
-                {
-                    document.getElementById('match').style.color='#dc3545' ;
-                    document.getElementById('match').innerHTML='Passwords do not match';
-                }
-            }
-
-            function checkPasswordStrength() 
-            {
-                        //toggle visibility
-                        if(document.getElementById('warning').style.display == 'none')
-                            document.getElementById('warning').style.display = 'block'; 
-                        
-                        if(document.getElementById('strength').style.display == 'none')
-                            document.getElementById('strength').style.display = 'block';
-                        
-                        if(document.getElementById('strength-desc').style.display == 'none')
-                            document.getElementById('strength-desc').style.display = 'block';
-                        
-
-                        var password=document.getElementById('password').value;
-
-                        //TextBox left blank.
-                        if (password.length == 0) 
-                        {
-                            document.getElementById('strength').innerHTML = "Password is Very Weak";
-                            document.getElementById('strength').style.color = '#dc3545' ;
-                            return;
-                        }
-
-
-                        //Regular Expressions.
-                        var regex = new Array();
-                        regex.push("[A-Z]"); //Uppercase Alphabet.
-                        regex.push("[a-z]"); //Lowercase Alphabet.
-                        regex.push("[0-9]"); //Digit.
-                        regex.push("[$@$!%*#?&]"); //Special Character.
-
-                        var passed = 0;
-
-                        //Validate for each Regular Expression.
-                        for (var i = 0; i < regex.length; i++) 
-                            if (new RegExp(regex[i]).test(password) && password.lenghth >=0) 
-                                passed++;
-
-                        //Validate for length of Password.
-                        if (passed > 2 && password.length > 8) 
-                            passed++;
-
-                        //Display status.
-                        var color = "";
-                        var strength = "";
-                        switch (passed) 
-                        {
-                            case 0:
-                                strength = "Very Weak";
-                                color = "#dc3545";
-                                break;
-                            case 1:
-                                strength = "Weak";
-                                color = "#dc3545";
-                                break;
-                            case 2:
-                                strength = "Good";
-                                color = "#ffc107";
-                                break;
-                            case 3:
-                                strength = "Strong";
-                                color = "#28a745";
-                                break;
-                            case 4:
-                                strength = "Very Strong";
-                                color = "darkgreen";
-                                break;
-                        }
-                        if(passed>2)
-                            document.getElementById('strength-desc').style.display = 'none';
-                            
-                        document.getElementById('strength').innerHTML = "Password is "+strength;
-                        
-                        document.getElementById('strength').style.color = color;
-            }
-     
-
-            function fvalidation()                                    
-            { 
-                    var uname = document.forms["regform"]["uname"];               
-                    var email = document.forms["regform"]["email"];    
-                   
-                   
-                    if (uname.value == "")                                  
-                    { 
-                        window.alert("Please enter a username."); 
-                        uname.focus(); 
-                        return false; 
-                    } 
-                                  
-                    if (email.value == "")                                   
-                    { 
-                        window.alert("Please enter a valid e-mail address."); 
-                        email.focus(); 
-                        return false; 
-                    } 
-
-
-            }
-
-
-            function checkpass()
-            {
-                    var pwd1  = document.forms["regform"]["pass"];
-                    var pwd2  = document.forms["regform"]["pass-rpt"];
-                    var goodColor = "#66cc66";
-                    var badColor = "#ff6666";
-
-                    if(pwd1.value.length > 8)
+                //Store the password field objects into variables ...
+                var password = document.getElementById('pass');
+                var confirm  = document.getElementById('repassword');
+                //Store the Confirmation Message Object ...
+                var message = document.getElementById('confirm-message2');
+                //Set the colors we will be using ...
+                var good_color = "#66cc66";
+                var bad_color  = "#ff6666";
+                //Compare the values in the password field 
+                //and the confirmation field
+              
+                    if(password.value == confirm.value)
                     {
-                        pwd1.style.backgroundColor = goodColor;
-                        message.style.color = goodColor;
-                        message.innerHTML = "character number ok!"
+                        //The passwords match. 
+                        //Set the color to the good color and inform
+                        //the user that they have entered the correct password 
+                        confirm.style.backgroundColor = good_color;
+                        message.style.color           = good_color;
+                        message.innerHTML             = "Passwords Match!";
                     }
                     else
                     {
-                        pwd.style.backgroundColor = badColor;
-                        message.style.color = badColor;
-                        window.alert("Please enter a valid PaSSS."); 
-                        message.innerHTML = " you have to enter at least 6 digit!"
-                        return;
+                        //The passwords do not match.
+                        //Set the color to the bad color and
+                        //notify the user.
+                        confirm.style.backgroundColor = bad_color;
+                        message.style.color           = bad_color;
+                        message.innerHTML             = "Passwords Do Not Match!";
                     }
-
+                
             }
-
-
-
 
         </script> 
 
@@ -176,22 +110,7 @@
 
 	<body onload='document.regform.uname.focus()'>
 
-     <div class="col-md-4 order-md-2 mb-4" id="warning" style="display:none;position:absolute;top:60%;transform:translateY(-60%);right:1%;transform:translateY(-1%);z-index: 1;">
-                    <ul class="list-group mb-3">
-                        <li class="list-group-item d-flex justify-content-between lh-condensed">
-                            <div>
-                                <h6 class="my-0" id="strength" style="display:none;"></h6>
-                                <small class="text-muted"  id="strength-desc" style="display:none;">Your password must be at least 8 characters long, contain at least one number and have a mixture of uppercase and lowercase letters.</small>
-                                <br>
-                                <h6 class="my-0" id="match" style="display:none;"></h6>
-                                <small class="text-muted" id="match-desc" style="display:none;">Please make sure both passwords are the same.</small>                                
-                            </div>
-                        </li>
-                    </ul>
-
-    </div>
-
-    <div class="header">
+   <div class="header">
 	   <h1>
             Hello Friend
        </h1>
@@ -215,7 +134,7 @@
     	</div>
 	</div>
 
-    <form name=regform action="signup_action.php" class="needs-validation" onsubmit="return fvalidation()"  method="post" novalidate>
+    <form name=regform action="signup_action.php" method="post">
         <div class="container" align=center>
             <h1>Register</h1>
             <p>Please fill in this form to create an account.</p>
@@ -230,64 +149,30 @@
             <br>
 
             <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" required>   
+            <input type="text" placeholder="Enter Email" id=email name="email" required >   
                     
             <br>
 
             <label for="pass"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" minlength=8 name="pass" id=password onkeyup="checkPass(); return false;" >
+            <input type="password" placeholder="Enter Password" minlength=8 name="pass" id=pass required onkeyup="checkPass();">
                 
             <br>
             <label for="pass-rpt"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" minlenghth=8 name="pass-rpt" id=repassword onkeyup="checkPass(); return false;">
+            <input type="password" placeholder="Repeat Password" minlenghth=8 name="pass-rpt" id=repassword required onkeyup="checkPass();">
             <br>   
             <hr>
             <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
-            <button type="submit" class="registerbtn" onclick="ValidateEmail(document.regform.email)">Register</button>
+            <button type="submit" class="registerbtn" onclick="ValidateEmail(document.regform.email,document.regform.pass,document.regform.pass-rpt)">Register</button>
         </div>
                       
         <div class="container signin">
             <p>Already have an account? <a href="#">Sign in</a>.</p>
         </div>
     </form>
-
-
-
-    <script>
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-        (
-        function() 
-        {
-            'use strict';
-
-            window.addEventListener('load', function() 
-            {
-                            // Fetch all the forms we want to apply custom Bootstrap validation styles to
-                var forms = document.getElementsByClassName('needs-validation');
-
-                            // Loop over them and prevent submission
-                var validation = Array.prototype.filter.call(forms, function(form) 
-                {
-                    form.addEventListener('submit', function(event) 
-                    {
-                        if (form.checkValidity() === false) 
-                        {
-                            event.preventDefault();
-                            event.stopPropagation()
-                        }
-                        form.classList.add('was-validated');
-                    }, false);
-                });
-
-            }, false);
-            
-        })();
-
+    <script src="mailvalidation.js">
 
     </script>
-
-    <script src="./js/mailvalidation.js"></script>
 
 </body>
 
